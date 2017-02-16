18797ed2e21e64d345aa57adc763416bf56efebf
TripleMan29 <den11051105@bk.ru>
2017-02-16 14:40:19 +0300

    1245234

--- PrefixTree.java
+++ PrefixTree.java
@@ -3,14 +3,11 @@ import java.util.List;
  * Created by Денис on 08.02.2017.
  */
 public class PrefixTree {
-    public static void main(List[] wordList) {
-        for (int i = 1; i < wordList.length; i++){
-            String word = wordList[i].toString();
+    public static void main(List[] List) {
+        for (int i = 1; i < List.length; i++){
+            String word = List[i].toString();
             String letters = word.split("").toString();
             if (letters.equals(" ")) break;
         }
-    }
-    public static void insert(String[] args) {
-
     }
 }
