e30bfea9629f9337b02b45db15dcdae880a30f93
TripleMan29 <den11051105@bk.ru>
2017-03-11 21:09:08 +0300

    Изменил немного тесты
    Попытался реализовать поиск по префиксам, но пока безуспешно(

--- Trie.java
+++ Trie.java
@@ -1,18 +1,21 @@
+import sun.reflect.generics.tree.Tree;
+
 import java.util.*;
 
 class TrieNode {
-    char content;
-    boolean isEnd;
-    int count;
+    public char content;
+    public boolean isEnd;
+    public int count;
     LinkedList<TrieNode> childList;
 
-    public TrieNode(char c){
+    TrieNode(char c) {
         childList = new LinkedList<TrieNode>();
         isEnd = false;
         content = c;
         count = 0;
     }
-    public TrieNode subNode(char c){
+
+    TrieNode subNode(char c) {               // Ищет у родителя дочернюю ветку с входным параметром char и выводит дочернюю ветку или null если ее нет
         if (childList != null)
             for (TrieNode eachChild : childList)
                 if (eachChild.content == c)
@@ -22,18 +25,19 @@ class TrieNode {
 }
 
 class Trie {
-    private TrieNode root;
+    public TrieNode root;
 
-    public Trie(){
+    Trie() {
         root = new TrieNode(' ');
     }
-    // Insertion
-    public void insert(String word){
-        if (search(word) == true)
+
+    // Addition
+    void insert(String word) {
+        if (search(word))
             return;
         TrieNode current = root;
-        for (char ch : word.toCharArray()){
-            TrieNode child = current.subNode(ch);
+        for (char ch : word.toCharArray()) {
+            TrieNode child = current.subNode(ch); // есть ли последующая буква в дереве, если нет то добавить в childList.add ниже написано если нет то родительский элемент равен дочернему
             if (child != null)
                 current = child;
             else {
@@ -44,37 +48,74 @@ class Trie {
         }
         current.isEnd = true;
     }
+
     // Searching
-    public boolean search(String word){
+    boolean search(String word) {
         TrieNode current = root;
-        for (char ch : word.toCharArray()){
+        for (char ch : word.toCharArray()) {
             if (current.subNode(ch) == null)
                 return false;
             else
                 current = current.subNode(ch);
         }
-        if (current.isEnd == true)
-            return true;
-        return false;
+        return current.isEnd;
     }
+
     // Removing
-    public void remove(String word){
-        if (search(word) == false){
-            System.out.println(word +" does not exist in trie");
-            return;
-        }
+    void remove(String word) {
+        if (!search(word)) throw new IllegalArgumentException("Element isnt found");
         TrieNode current = root;
-        for (char ch : word.toCharArray()){
+        for (char ch : word.toCharArray()) {
             TrieNode child = current.subNode(ch);
-            if (child.count == 1){
+            if (child.count == 1) {
                 current.childList.remove(child);
                 return;
-            }
-            else {
+            } else {
                 child.count--;
                 current = child;
             }
         }
         current.isEnd = false;
     }
+
+
+    // Prefix Searching
+    public String prefixSearch(String pref) {
+        TrieNode current = root;
+        for (char ch : pref.toCharArray()) {
+            TrieNode child = current.subNode(ch);
+            current = child;
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < current.count; i++) {
+            TrieNode child = current.childList.get(i);
+            char[] word = new char[50];
+            int j=0;
+            do {
+                word[j] = child.content;
+                j++;
+                child = child.childList.getFirst();
+            } while (!child.childList.isEmpty());
+            word[j] = child.content;
+            result.append(", ").append(pref).append(word);
+            if (result.length() > 1) result.delete(0, 2);
+        }
+        return result.toString();
+    }
+
+  /*  @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Trie trie = (Trie) o;
+
+        return root != null ? root.equals(trie.root) : trie.root == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return root != null ? root.hashCode() : 0;
+    }*/
 }
--- TrieTest.java
+++ TrieTest.java
@@ -3,36 +3,35 @@ import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TrieTest {
+public class TrieTest{
     static Trie prefTree;
-    @BeforeClass
-    public static void initial(){
+    @Test
+    public void initial(){
         prefTree = new Trie();
         prefTree.insert("House");
         prefTree.insert("Mum");
         prefTree.insert("Board");
-    }
 
-    @Test
-    public void search1(){
         assertEquals(true, prefTree.search("House"));
         assertEquals(false, prefTree.search("Home"));
         assertEquals(true, prefTree.search("Mum"));
         assertEquals(true, prefTree.search("Board"));
+        assertEquals(true, prefTree.search("Board"));
     }
 
-    @After
+    @Test
     public void remove(){
+        prefTree = new Trie();
+        prefTree.insert("House");
+        prefTree.insert("Mum");
+        prefTree.insert("Board");
         prefTree.remove("House");
         prefTree.remove("Mum");
         prefTree.remove("Board");
         prefTree.insert("Home");
         prefTree.insert("Car");
         prefTree.insert("Dad");
-    }
 
-    @Test
-    public void search2(){
         assertEquals(false, prefTree.search("House"));
         assertEquals(true, prefTree.search("Home"));
         assertEquals(true, prefTree.search("Car"));
